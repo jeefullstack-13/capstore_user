cf463a15b5538982b0c2102ea91e23f888f0bf78
Training2017 <vijayalakshmi.david@capgemini.com>
2018-08-14 16:08:54 +0530

    first commit

---
+++ src/main/java/org/capstore/user/CapstoreUserApplication.java
@@ -0,0 +1,12 @@
+package org.capstore.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoreUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoreUserApplication.class, args);
+	}
+}
