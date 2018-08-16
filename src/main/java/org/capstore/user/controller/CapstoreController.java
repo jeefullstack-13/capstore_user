3a3bdac9b32d6eb789f92e5a8da3290d80514da2
askommar <askommar@DIN73002746.corp.capgemini.com>
2018-08-16 10:35:03 +0530

    HomePage First Commit

---
+++ src/main/java/org/capstore/user/controller/CapstoreController.java
@@ -0,0 +1,44 @@
+package org.capstore.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CapstoreController {
+
+	@RequestMapping("/CapStoreAfterLogin")
+	public String capStoreAfterLogin() {
+		
+		return "CapStoreAfterLogin";
+	}
+	
+	@RequestMapping("/CapStoreHome_full")
+	public String capStoreHomeFull() {
+		
+		return "CapStoreHome_full";
+	}
+	
+	@RequestMapping("/CapStoreHome")
+	public String capStoreHome() {
+		
+		return "CapStoreHome";
+	}
+	
+	@RequestMapping("/Page1_full")
+	public String page1() {
+		
+		return "Page1_full";
+	}
+	
+	@RequestMapping("/product")
+	public String product() {
+		
+		return "product";
+	}
+	
+	@RequestMapping("/Product_full")
+	public String productFull() {
+		
+		return "Product_full";
+	}
+}
