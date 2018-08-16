0e7e5bce4b3db42ea3dc593b183f81eb4e82039b
sypuppal <sypuppal@DIN73002974.corp.capgemini.com>
2018-08-16 16:12:53 +0530

    Adding return order page

--- src/main/java/org/capstore/user/controller/CustomerController.java
+++ src/main/java/org/capstore/user/controller/CustomerController.java
@@ -1,7 +1,10 @@
 package org.capstore.user.controller;
 
+
+import org.capstore.user.model.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class CustomerController {
@@ -17,5 +20,14 @@ public class CustomerController {
 	public String capStoreHomeFull() {
 		
 		return "customHomePage";
+	}
+	@RequestMapping("/order")
+	public String orders() {
+		final String uri="http://localhost:8081/PilotRestApp/api/v1/pilots";
+		RestTemplate restTemplate=new RestTemplate();
+		
+		Order[] orders= restTemplate.getForObject(uri, Order[].class);
+		
+		return "order";
 	}
 }
