73d2e52a08b6ba4b05b4963e58c017690ff17539
kavysv <kavysv@DIN73002994.corp.capgemini.com>
2018-08-20 18:05:59 +0530

    customer commit

--- src/main/java/org/capstore/user/controller/CustomerController.java
+++ src/main/java/org/capstore/user/controller/CustomerController.java
@@ -1,15 +1,12 @@
 package org.capstore.user.controller;
 
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
-
 import org.capstore.user.model.Customer;
 import org.capstore.user.model.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
@@ -52,19 +49,55 @@ public class CustomerController {
 			return "customerorder";
 			}
 	
-	@PutMapping("/edit")
-	public String editProfile(	@Valid @ModelAttribute("cust") Customer cust,
-			BindingResult result) {
-		
-				if(!result.hasErrors()) {
-			final String uri="http://localhost:8085/capstoreApp/api/v1//profile/{customerId}";
-			RestTemplate restTemplate=new RestTemplate();
-			restTemplate.postForEntity(uri,cust,Customer.class);
-					}
+	@RequestMapping("/customerProfile/{customerId}")
+	public String viewCustomerProfile(HttpSession session,ModelMap map) {
+		//Integer custId= Integer.parseInt(session.getAttribute("customerId").toString());
 
-		return "customerProfile"; 
+		final String uri="http://localhost:8085/capstoreApp/api/v1/customerProfile/1";
+		RestTemplate restTemplate=new RestTemplate();
+		
+		Customer customer= restTemplate.getForObject(uri, Customer.class);
+		
+	
+		map.put("cust",customer);
+				
+			return "customerProfile";
+			}
+	
+	@RequestMapping("/profile")
+	public String editCustomerProfile(ModelMap map) {
+		
+		final String uri="http://localhost:8085/capstoreApp/api/v1/customerProfile";
+		RestTemplate restTemplate=new RestTemplate();
+		
+		map.put("cust", new Customer());
+		
+		return "redirect:profile";
 		
 	}
+	
+	
+	@RequestMapping("/update")
+	public String editProfile(ModelMap map) {
+		
+		/*final String uri="http://localhost:8085/capstoreApp/api/v1";
+		RestTemplate restTemplate=new RestTemplate();
+		Customer[] customer= restTemplate.getForObject(uri, Customer[].class);
+		
+		RestTemplate restTemplate1=new RestTemplate();
+			final String uri1="http://localhost:8085/capstoreApp/api/v1//profile/{customerId}";
+			Customer customer1=	restTemplate.getForObject(uri,Customer.class);
+			map.put("customers",customer);
+			map.put("cust", customer1);*/
+			
+			return "customerProfile"; 
+		
+	}
+	
+	
+	
+	
+	
 	
 	
 }
